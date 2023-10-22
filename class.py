21d526ca64fd0b68730cd039435cdbda7799c254
sarveshjadhav03 <sarveshjadhav.mituniversity@gmail.com>
2023-10-22 19:26:09 +0530

    first commit code

---
+++ class.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name,age,marks):
+            self.name=name
+            self.age=age
+            self.marks=marks
+            return 
+    
+S1=Student("sarvesh",21,89)
+print(Student(S1))
